dc16bc2bf754668990e4eed1650263ceeb4c60bf
RukminiBiradar <109748560+RukminiBiradar@users.noreply.github.com>
2022-08-18 13:24:20 +0530

    Add files via upload

---
+++ Day5/LeapYear.java
@@ -0,0 +1,20 @@
+package Day5Programs;
+
+import java.util.Scanner;
+
+public class LeapYear 
+{	
+	public static void main(String args[])
+	{
+	Scanner input=new Scanner(System.in);
+		
+		System.out.println("enter year");
+		int n=input.nextInt();
+		
+		 if (((n % 4 == 0) && (n % 100!= 0)) || (n%400 == 0))
+			 System.out.println("leap year");
+		 else
+			 System.out.println("not leap year");
+	}
+
+}
