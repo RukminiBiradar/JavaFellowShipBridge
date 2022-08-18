5c5f9bb889b1530a562e2dea4fc617b8845e6853
RukminiBiradar <109748560+RukminiBiradar@users.noreply.github.com>
2022-08-18 13:22:11 +0530

    Add files via upload

---
+++ Day3/EmpWageUc1.java
@@ -0,0 +1,17 @@
+package Day3;
+public class EmpWageUc1 
+{
+	public static void main(String[] args) 
+	{
+		int Is_full_Time=1;
+		
+		double randomCheck=Math.floor(Math.random()*10)%2;
+		if(randomCheck==Is_full_Time)
+			System.out.println("employee is present");
+		else
+			System.out.println("employee is not present");
+		
+		
+	}
+
+}
---
+++ Day3/EmpWageUc6.java
@@ -0,0 +1,33 @@
+package Day3;
+
+public class EmpWageUc6 
+{
+	public static final int Is_Part_Time=1;
+	public static final int IS_Full_TIME =2;
+	public static final int EMP_RATE_PER_HOUR =20;
+	public static final int NUM_OF_WORKING_DAYS =2;
+	public static final int MAX_HRS_IN_MONTH = 10;
+	
+	public static void main(String args[ ]){
+		int empHrs=0, totalEmpHrs = 0,totalEmpWage=0,empWage=0, totalWorkingDays=0 ;
+		for (int day = 0; day < NUM_OF_WORKING_DAYS; day++){
+			int empCheck= (int) Math.floor(Math.random() * 10) % 3;
+			switch (empCheck){
+			case Is_Part_Time:
+				empHrs = 4;
+				break;
+			case IS_Full_TIME:
+				empHrs = 8;
+				break;
+			default:
+				empHrs = 0;
+			}
+			empWage=empHrs*EMP_RATE_PER_HOUR;
+			totalEmpWage += empWage;
+			System.out.println("Employee wages is:" + empWage);
+}
+System.out.println("Employee wages is:" + totalEmpWage);		
+}
+}
+
+
